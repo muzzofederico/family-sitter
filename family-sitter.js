77d8d39f3dd785c18458fab7e07ac63b3952efb0
muzzofederico <muzzofederico@users.noreply.github.com>
2016-01-18 17:25:15 +0100

    Un botto di roba

--- family-sitter.js
+++ family-sitter.js
@@ -1,24 +1,40 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  /* Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  }); */
+	// counter starts at 0
+	Session.setDefault('counter', 0);
+	
+	Template.navigation.helpers({
+		domain: function() {
+			return Meteor.absoluteUrl();
+		}
+	});
+	
+	Template.navigation.onRendered(function () {
+		$('body').flowtype({
+			minFont   : 14,
+			maxFont   : 16,
+			fontRatio : 30
+		});
+		
+		$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top - 50
+				}, 500);
+				return false;
+			  }
+			}
+		  });
+		});
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-  var utenti = new Mongo.Collection('users');
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
+	var utenti = new Mongo.Collection('users');
 }
